8b968c5d10179c990ec14aab68283062d8647204
Ladlen <TwilightTower@mail.ru>
2017-08-20 01:46:44 +0300

    first commit

---
+++ car-calculator.php
@@ -0,0 +1,359 @@
+<?php
+/**
+ * Plugin Name: Калькулятор цен автомобилей
+ * Description: Отправляет заявку на расчет цена на автомобили.
+ * Version: 1.0
+ */
+
+
+function wnd_calc_scripts()
+{
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('jquery-effects-core');
+}
+add_action('wp_enqueue_scripts', 'wnd_calc_scripts');
+
+
+add_action('admin_menu', 'plugin_admin_add_car_calc_page');
+function plugin_admin_add_car_calc_page()
+{
+    add_options_page('Настройки калькулятора автомобилей', 'Калькулятор автомобилей', 'manage_options', 'windows_calculator', 'plugin_options_page');
+}
+
+function plugin_options_page()
+{
+    ?>
+    <div class="wrap">
+        <h2>Расчет цены окон</h2>
+
+        <form action="options.php" method="post">
+            <?php settings_fields('plugin_options_wnd_calc'); ?>
+            <?php do_settings_sections('windows_calculator'); ?>
+
+            <input name="Submit" type="submit" value="<?php esc_attr_e('Сохранить изменения'); ?>"/>
+        </form>
+    </div>
+
+    <?php
+}
+
+$defaultWndCalcOptions = [
+    'profile' => [
+        'name' => ['REHAU BLITZ', 'REHAU INTELIO', 'REHAU BRILLANT', 'REHAU DELIGHT', 'REHAU SIB', 'REHAU GENEO'],
+        'price' => [1.3, 1.4, 1.5, 1.6, 1.7, 1.8],
+    ],
+    'furniture' => [
+        'name' => ['Нет', 'ROTO', 'VORNE', 'SIGENIA'],
+        'price' => [1, 1.1, 1.2, 1.3],
+    ],
+    'dglazed' => [
+        'name' => ['Стандартный', 'Энергосберегающий'],
+        'price' => [2, 2.3],
+    ],
+    'sill' => [
+        'name' => ['Нет', '200 мм', '250 мм', '300 мм', '350 мм', '400 мм', '500 мм'],
+        'price' => [0, 101, 102, 103, 104, 105, 106],
+    ],
+    'otliv' => [
+        'name' => ['Нет', '100 мм', '150 мм', '200 мм', '250 мм', '300 мм', '350 мм', '400 мм'],
+        'price' => [0, 111, 112, 113, 114, 115, 116, 117],
+    ],
+    'setting' => [
+        'name' => ['Нет', 'Стандарт', 'ГОСТ'],
+        'price' => [0, 2000, 3000],
+    ],
+    'slopes' => [
+        'name' => ['Нет', '200 мм', '250 мм', '300 мм', '350 мм', '400 мм', '500 мм'],
+        'price' => [0, 101, 102, 103, 104, 105, 106],
+    ],
+    'accessories' => [
+        'name' => ['Москитная сетка', 'Детский замок', 'Гребенка'],
+        'price' => [510, 620, 730],
+    ],
+];
+
+
+//delete_option('plugin_options_wnd_calc');
+
+// add the admin settings and such
+add_action('admin_init', 'plugin_admin_init');
+function plugin_admin_init()
+{
+    register_setting('plugin_options_wnd_calc', 'plugin_options_wnd_calc', 'plugin_options_validate_wnd_calc');
+    add_settings_section('plugin_main_wnd_calc', 'Настройки', 'plugin_section_text_wnd_calc', 'windows_calculator');
+
+    add_settings_field('plugin_wnd_calc_window', 'Тип окон', 'plugin_wnd_calc_window_func', 'windows_calculator', 'plugin_main_wnd_calc');
+    add_settings_field('plugin_wnd_calc_profile', 'Профиль', 'plugin_wnd_calc_profile_func', 'windows_calculator', 'plugin_main_wnd_calc');
+    add_settings_field('plugin_wnd_calc_furniture', 'Фурнитура', 'plugin_wnd_calc_furniture_func', 'windows_calculator', 'plugin_main_wnd_calc');
+    add_settings_field('plugin_wnd_calc_dglazed', 'Стеклопакет', 'plugin_wnd_calc_dglazed_func', 'windows_calculator', 'plugin_main_wnd_calc');
+    add_settings_field('plugin_wnd_calc_setting', 'Установка', 'plugin_wnd_calc_setting_func', 'windows_calculator', 'plugin_main_wnd_calc');
+    add_settings_field('plugin_wnd_calc_sill', 'Подоконник', 'plugin_wnd_calc_sill_func', 'windows_calculator', 'plugin_main_wnd_calc');
+    add_settings_field('plugin_wnd_calc_otliv', 'Отлив', 'plugin_wnd_calc_otliv_func', 'windows_calculator', 'plugin_main_wnd_calc');
+    add_settings_field('plugin_wnd_calc_slopes', 'Откосы', 'plugin_wnd_calc_slopes_func', 'windows_calculator', 'plugin_main_wnd_calc');
+    add_settings_field('plugin_wnd_calc_accessories', 'Комплектующие', 'plugin_wnd_calc_accessories_func', 'windows_calculator', 'plugin_main_wnd_calc');
+
+    wp_enqueue_script('jquery');
+
+    wp_enqueue_style('wnd_calc_admin_style', plugins_url('', __FILE__) . '/admin.css');
+    wp_enqueue_script('wnd_calc_admin_script', plugins_url('', __FILE__) . '/admin.js', ['jquery']);
+
+    wp_enqueue_media();
+}
+
+function plugin_section_text_wnd_calc()
+{
+    //echo '<p>Main description of this section here.</p>';
+}
+
+function plugin_wnd_calc_accessories_func()
+{
+    showChangeOptionTable('accessories');
+}
+
+function plugin_wnd_calc_slopes_func()
+{
+    showChangeOptionTable('slopes', 'price_for_window_width_height');
+}
+
+function plugin_wnd_calc_furniture_func()
+{
+    showChangeOptionTable('furniture', 'koeff_window_price');
+}
+
+function plugin_wnd_calc_dglazed_func()
+{
+    showChangeOptionTable('dglazed', 'koeff_window_price');
+}
+
+function plugin_wnd_calc_setting_func()
+{
+    showChangeOptionTable('setting', 'price_for_window_square');
+}
+
+function plugin_wnd_calc_otliv_func()
+{
+    showChangeOptionTable('otliv', 'price_for_window_width');
+}
+
+function plugin_wnd_calc_sill_func()
+{
+    showChangeOptionTable('sill', 'price_for_window_width');
+}
+
+
+function plugin_wnd_calc_profile_func()
+{
+    showChangeOptionTable('profile', 'koeff_window_price');
+}
+
+function plugin_wnd_calc_window_func()
+{
+    $options = get_option('plugin_options_wnd_calc', $GLOBALS['defaultWndCalcOptions']);
+    $s = '<table class="wnd_calc_wnd_options wnd_calc_wnd_options_wnd"><thead><tr><th>Название</th><th>Цена</th><th>&nbsp;</th><th>Высота</th><th>Мин. высота</th><th>Макс. высота</th><th>Панелей</th><th><input class="calc_wnd_option_type" type="hidden" value="window"></th></tr></thead><tbody>';
+    if (!empty($options['window'])) {
+        foreach ($options['window']['name'] as $key => $pf) {
+            $panes = isset($options['window']['panes']['width'][$key]) ? $options['window']['panes']['width'][$key] : [];
+            $s .= '<tr>'
+                . '<td>'
+                . '<span class="wnd_small">Название</span><br>'
+                . '<input class="name_wnd_option name_wnd_option_not_short" type="text" value="' . esc_html($pf) . '" name="plugin_options_wnd_calc[window][name][' . $key . ']" readonly="readonly">'
+                . '<input type="checkbox" class="mod_wnd_option_name" title="Редактировать">'
+                . '</td>'
+                . '<td>'
+                . '<span class="wnd_small">Цена</span><br>'
+                . '<input class="name_wnd_option_short" type="text" value="' . esc_html($options['window']['price'][$key]) . '" name="plugin_options_wnd_calc[window][price][' . $key . ']" readonly="readonly">'
+                . '<input type="checkbox" class="mod_wnd_option_price" title="Редактировать">'
+                . '</td>'
+                . '<td class="tbl_center">'
+                . '<input type="hidden" value="' . esc_html($options['window']['id_small'][$key]) . '" name="plugin_options_wnd_calc[window][id_small][' . $key . ']">'
+                . '<input type="hidden" value="' . esc_html($options['window']['src_small'][$key]) . '" name="plugin_options_wnd_calc[window][src_small][' . $key . ']">'
+                . '<img alt="" src="' . esc_html($options['window']['src_small'][$key]) . '" class="mod_wnd_option_class_preview_image" title="Нажмите чтобы увеличить">'
+                . '<div><button class="mod_wnd_option_change_preview_image" title="Добавить/изменить маленькое окно">Мал. окно</button></div>'
+                . '</td>'
+                . '<td>'
+                . '<span class="wnd_small">Высота</span><br>'
+                . '<input class="name_wnd_option_short" type="text" value="' . esc_html($options['window']['height'][$key]) . '" name="plugin_options_wnd_calc[window][height][' . $key . ']" readonly="readonly">'
+                . '<input type="checkbox" class="mod_wnd_option_name" title="Редактировать">'
+                . '</td>'
+                . '<td>'
+                . '<span class="wnd_small">Мин. высота</span><br>'
+                . '<input class="name_wnd_option_short" type="text" value="' . esc_html($options['window']['height-min'][$key]) . '" name="plugin_options_wnd_calc[window][height-min][' . $key . ']" readonly="readonly">'
+                . '<input type="checkbox" class="mod_wnd_option_name" title="Редактировать">'
+                . '</td>'
+                . '<td>'
+                . '<span class="wnd_small">Макс. высота</span><br>'
+                . '<input class="name_wnd_option_short" type="text" value="' . esc_html($options['window']['height-max'][$key]) . '" name="plugin_options_wnd_calc[window][height-max][' . $key . ']" readonly="readonly">'
+                . '<input type="checkbox" class="mod_wnd_option_name" title="Редактировать">'
+                . '</td>'
+                . '<td>'
+                . '<span class="wnd_small">Панелей</span><br>'
+                . '<input class="name_wnd_option_short add_window_pane" type="number" min="1" max="20" value="' . count($panes) . '" readonly="readonly">'
+                . '<input type="checkbox" class="mod_wnd_option_name" title="Редактировать">'
+                . '</td>'
+                . '<td class="tbl_center"><button data-type="window" class="rem_wnd_option">Удалить</button></td>'
+                . '</tr>';
+
+            $s .= '<tr><td colspan="8" style="text-align: right">';
+
+            if ($panes) {
+                foreach ($panes as $keyPane => $pane) {
+                    $subtypes = isset($options['window']['panes']['subtypes']['id_image'][$key][$keyPane]) ? $options['window']['panes']['subtypes']['id_image'][$key][$keyPane] : [];
+
+                    $s .= '<table style="width:90%;float:right;">'
+                        . '<thead>'
+                        . '<tr>'
+                        . '<th>'
+                        . 'Ширина:<br><input class="name_wnd_option_short" type="text" value="' . esc_html($pane) . '" name="plugin_options_wnd_calc[window][panes][width][' . $key . '][' . $keyPane . ']" readonly="readonly">'
+                        . '<input type="checkbox" class="mod_wnd_option_name" title="Редактировать">'
+                        . '</th>'
+                        . '<th>'
+                        . 'Мин. ширина:<br><input class="name_wnd_option_short" type="text" value="' . esc_html($options['window']['panes']['width-min'][$key][$keyPane]) . '" name="plugin_options_wnd_calc[window][panes][width-min][' . $key . '][' . $keyPane . ']" readonly="readonly">'
+                        . '<input type="checkbox" class="mod_wnd_option_name" title="Редактировать">'
+                        . '</th>'
+                        . '<th>'
+                        . 'Макс. ширина:<br><input class="name_wnd_option_short" type="text" value="' . esc_html($options['window']['panes']['width-max'][$key][$keyPane]) . '" name="plugin_options_wnd_calc[window][panes][width-max][' . $key . '][' . $keyPane . ']" readonly="readonly">'
+                        . '<input type="checkbox" class="mod_wnd_option_name" title="Редактировать">'
+                        . '</th>'
+                        . '<th>'
+                        . 'Цена:<br><input class="name_wnd_option_short" type="text" value="' . esc_html($options['window']['panes']['price'][$key][$keyPane]) . '" name="plugin_options_wnd_calc[window][panes][price][' . $key . '][' . $keyPane . ']" readonly="readonly">'
+                        . '<input type="checkbox" class="mod_wnd_option_name" title="Редактировать">'
+                        . '</th>'
+                        . '<th>Количество подтипов:<br>'
+                        . '<input class="name_wnd_option_short add_window_subpane" type="number" min="1" max="20" value="' . count($subtypes) . '" readonly="readonly">'
+                        . '<input type="checkbox" class="mod_wnd_option_price" title="Редактировать">'
+                        . '</th>'
+                        . '</tr>'
+                        . '</thead>'
+                        . '<tbody>';
+
+                    foreach ($subtypes as $keySubtype => $sbtype) {
+                        $src = $options['window']['panes']['subtypes']['src_image'][$key][$keyPane][$keySubtype];
+                        $price = $options['window']['panes']['subtypes']['price'][$key][$keyPane][$keySubtype];
+                        $name = $options['window']['panes']['subtypes']['name'][$key][$keyPane][$keySubtype];
+                        $s .= '<tr>'
+                            . '<td colspan="2" style="text-align:right">'
+                            . 'Имя: <input type="text" value="' . esc_html($name) . '" name="plugin_options_wnd_calc[window][panes][subtypes][name][' . $key . '][' . $keyPane . '][]" readonly="readonly">'
+                            . '<input type="checkbox" class="mod_wnd_option_name" title="Редактировать">'
+                            . '</td>'
+                            . '<td colspan="2" style="text-align:right">'
+                            . 'Цена м<sup>2</sup>: <input type="text" value="' . esc_html($price) . '" name="plugin_options_wnd_calc[window][panes][subtypes][price][' . $key . '][' . $keyPane . '][]" readonly="readonly">'
+                            . '<input type="checkbox" class="mod_wnd_option_name" title="Редактировать">'
+                            . '</td>'
+                            . '<td style="text-align:right">'
+                            . '<input type="hidden" value="' . esc_html($sbtype) . '" name="plugin_options_wnd_calc[window][panes][subtypes][id_image][' . $key . '][' . $keyPane . '][]">'
+                            . '<input type="hidden" value="' . esc_html($src) . '" name="plugin_options_wnd_calc[window][panes][subtypes][src_image][' . $key . '][' . $keyPane . '][]">'
+                            . '<img alt="" src="' . esc_html($src) . '" class="mod_wnd_option_class_preview_image big" title="Нажмите чтобы увеличить">'
+                            . '<div><button class="mod_wnd_option_change_preview_image" title="Добавить/изменить изображение">Изображение</button></div>'
+                            . '</td>'
+                            . '</tr>';
+                    }
+
+                    $s .= '</tbody>';
+
+                    $s .= '</table>';
+                }
+            } else {
+                $s .= '&nbsp;';
+            }
+            $s .= '</td></tr>';
+        }
+    }
+
+    $s .= '</tbody></table><button class="wnd_calc_wnd_option_add_window">Добавить</button>';
+
+    echo $s;
+}
+
+/**
+ * @param $id
+ * @param string $priceType тип цены: [
+ *      'koeff_window_price', - коэффициент удорожания от стоимости окна
+ *      'price_for_window_square', - цена за площадь окна (кв. м.)
+ *      'price_for_window_width', - цена за ширину окна (п. м.)
+ *      'price_for_window_width_height', - цена за ширину окна + высота блоков (п. м.)
+ *      'price_for_item' - цена за штуку
+ *  ]
+ */
+function showChangeOptionTable($id, $priceType = 'price_for_item')
+{
+    $options = get_option('plugin_options_wnd_calc', $GLOBALS['defaultWndCalcOptions']);
+
+    switch ($priceType)
+    {
+        case 'koeff_window_price':
+            $priceCaption = 'Коэфф. от ст-сти окна';
+            break;
+        case 'price_for_window_square':
+            //, - цена за площадь окна (кв. м.)
+            $priceCaption = 'Цена за кв.м. окна';
+            break;
+        case 'price_for_window_width':
+            //, - цена за ширину окна (п. м.)
+            $priceCaption = 'Цена за п.м. окна';
+            break;
+        case 'price_for_window_width_height':
+            //, - цена за ширину окна + высота блоков (п. м.)
+            $priceCaption = 'Цена за ширину и высоту окна';
+            break;
+        default:
+            $priceCaption = 'Цена за шт.';
+            break;
+    }
+
+    //echo "<input id='plugin_wnd_calc_profile' name='plugin_options_wnd_calc[text_string]' size='40' type='text' value='{$options['text_string']}' />";
+    $s = '<table class="wnd_calc_wnd_options"><thead><tr><th>Название</th><th>' . $priceCaption . '</th><th><input class="calc_wnd_option_type" type="hidden" value="' . $id . '"></th></tr></thead><tbody>';
+    if (!empty($options[$id])) {
+        foreach ($options[$id]['name'] as $key => $pf) {
+            $s .= '<tr>'
+                . '<td>'
+                . '<input class="name_wnd_option" type="text" value="' . esc_html($pf) . '" name="plugin_options_wnd_calc[' . $id . '][name][]" readonly="readonly">'
+                . '<input type="checkbox" class="mod_wnd_option_name" title="Редактировать">'
+                . '</td>'
+                . '<td>'
+                . '<input type="text" value="' . esc_html($options[$id]['price'][$key]) . '" name="plugin_options_wnd_calc[' . $id . '][price][]" readonly="readonly">'
+                . '<input type="checkbox" class="mod_wnd_option_price" title="Редактировать">'
+                . '</td>'
+                . '<td class="tbl_center"><button class="rem_wnd_option">Удалить</button></td>'
+                . '</tr>';
+        }
+    }
+
+    $s .= '</tbody></table><button class="wnd_calc_wnd_option_add">Добавить</button>';
+
+    echo $s;
+}
+
+/*function plugin_options_validate_wnd_calc($input)
+{
+    $newInput = $input;
+    $newInput['text_string'] = trim($input['text_string']);
+    return $newInput;
+}*/
+
+//wp_enqueue_script('jquery');
+
+wp_register_style('wnd_calc_style', plugins_url('', __FILE__) . '/template.css');
+wp_register_style('wnd_calc_bpopup', plugins_url('', __FILE__) . '/bpopup.css');
+
+//wp_register_script('jquery-effects-core');    // wrong params
+wp_register_script('wnd_calc_script', plugins_url('', __FILE__) . '/template.js', ['jquery']);
+wp_register_script('wnd_calc_number_format_script', plugins_url('', __FILE__) . '/jquery.number.min.js', ['jquery']);
+wp_register_script('wnd_calc_order_popup_script', plugins_url('', __FILE__) . '/jquery.bpopup.min.js', ['jquery']);
+
+function shortcode_wnd_calc()
+{
+    wp_enqueue_style('wnd_calc_style');
+    wp_enqueue_style('wnd_calc_bpopup');
+
+    wp_enqueue_script('wnd_calc_script');
+    wp_enqueue_script('wnd_calc_number_format_script');
+    wp_enqueue_script('wnd_calc_order_popup_script');
+
+    ob_start();
+    require_once 'template.php';
+    $content = ob_get_contents();
+    ob_end_clean();
+    return $content;
+}
+
+add_shortcode('windows_calculator', 'shortcode_wnd_calc');
---
+++ template.php
@@ -0,0 +1,164 @@
+<?php
+
+class CarCalc
+{
+    public $windowSelect = '';
+    public $commonSelect = '';
+
+    public $options;
+
+    public function __construct()
+    {
+        $this->options = get_option('plugin_options_wnd_calc');
+    }
+
+    public function showWindows($key, $info)
+    {
+        $s = '';
+        foreach ($info['src_small'] as $key => $link) {
+            $s .= '<img class="wnd_calc_prev_window" src="' . esc_html($link) . '" alt="" data-id="' . esc_html($key)
+                . '" data-name="' . $info['name'][$key] . '" data-price="' . $info['price'][$key] . '">';
+        }
+        $this->windowSelect = $s;
+    }
+
+    public function showAccessories($optKey, $info)
+    {
+        $this->commonSelect .= '<div class="wnd_calc_select_option"><div style="margin-top:10px;margin-bottom:5px;font-size:18px;">Комплектующие:</div>';
+        foreach ($info['name'] as $key => $pf) {
+            $id = "wnd_calc_checkbox_{$optKey}_{$key}";
+            $this->commonSelect .= '<input class="wnd_calc_setting_checkbox" data-name="' . esc_html($pf) . '" data-price="' . esc_html($info['price'][$key]) . '" style="margin-left:96px" type="checkbox" id="' . esc_html($id) . '"><label class="wnd_sel_checkbox_label" for="' . esc_html($id) . '">' . esc_html($pf) . '</label><br>';
+        }
+        $this->commonSelect .= '</div>';
+    }
+
+    protected function showDropdown($name, $optKey, $info)
+    {
+        $selId = "wnd_calc_select_$optKey";
+        $this->commonSelect .= '<div class="wnd_calc_select_option"><label class="wnd_calc_so_label" for="' . $selId . '">' . esc_html($name) . '</label>'
+            . '<select class="wnd_calc_select" id="' . $selId . '">';
+        foreach ($info['name'] as $key => $pf) {
+            $this->commonSelect .= '<option value="' . esc_html($info['price'][$key]) . '">' . esc_html($pf) . '</option>';
+        }
+        $this->commonSelect .= '</select></div>';
+    }
+
+    public function init()
+    {
+        foreach ($this->options as $key => $info) {
+            switch ($key) {
+                case 'window':
+                    $this->showWindows($key, $info);
+                    break;
+                case 'profile':
+                    $this->showDropdown('Профиль', $key, $info);
+                    break;
+                case 'furniture':
+                    $this->showDropdown('Фурнитура', $key, $info);
+                    break;
+                case 'dglazed':
+                    $this->showDropdown('Стеклопакет', $key, $info);
+                    break;
+                case 'sill':
+                    $this->showDropdown('Подоконник', $key, $info);
+                    break;
+                case 'otliv':
+                    $this->showDropdown('Отлив', $key, $info);
+                    break;
+                case 'setting':
+                    $this->showDropdown('Установка', $key, $info);
+                    break;
+                case 'slopes':
+                    $this->showDropdown('Откосы', $key, $info);
+                    break;
+                case 'accessories':
+                    $this->showAccessories($key, $info);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+$CarCalc = new CarCalc();
+$CarCalc->init();
+
+?>
+<script>
+    var wndSelVariables = <?php echo json_encode($CarCalc->options) ?>;
+    var wndSelPluginPath = <?php echo json_encode(plugins_url('', __FILE__)) ?>;
+</script>
+
+<div class="wnd_calc_container">
+    <div class="wnd_calc_container_tr">
+        <div class="wnd_calc_select_wrapper">
+            <div class="wnd_calc_window_type">
+                <div class="wnd_calc_capt">Тип изделия</div>
+                <div class="wnd_calc_window_type_select"><?php echo $CarCalc->windowSelect ?></div>
+            </div>
+            <div class="wnd_calc_characteristics">
+                <div class="wnd_calc_capt">Характеристики</div>
+                <div class="wnd_calc_characteristic"><?php echo $CarCalc->commonSelect ?></div>
+            </div>
+        </div>
+        <div class="wnd_calc_result">
+            <div class="wnd_calc_capt wnd_calc_capt_sz">Размеры</div>
+            <div id="wnd_calc_window_preview">
+                Чтобы поменять тип открывания кликните на створку
+                <div class="wnd_calc_window_item"></div>
+                <div class="wnd_calc_hide_sizes_wrapper">
+                    <label><input type="checkbox" class="wnd_calc_hide_sizes" checked="checked">Показывать размеры</label>
+                </div>
+            </div>
+            <div class="wnd_calc_price">
+                <div class="wnd_calc_total">Всего:
+                    <div id="wnd_calc_price"></div>
+                    руб.
+                </div>
+                <button class="wnd_calc_order">Заказать</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--<div id="contact_popup">-->
+<!--<div style="position:fixed;top:0;left:0;width:100%;height:0;">-->
+<div id="wnd_calc_order_popup">
+    <span class="b-close"><span>X</span></span>
+
+    <form id="wnd_calc_order_form" method="post">
+        <input type="hidden" name="action" value="make_order">
+        <h2>Заказать</h2>
+        <br>
+        <div class="form-group">
+            <label for="order_name">Имя <span class="required">*</span></label>
+            <input type="text" class="form-control" id="order_name" name="name" placeholder="Имя">
+            <small class="help-block no_name" style="display:none">Это поле необходимо заполнить</small>
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="order_email">E-mail <span class="required">*</span></label>
+            <input type="email" class="form-control" id="order_email" name="email" placeholder="E-mail">
+            <small class="help-block email" style="display:none"></small>
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="order_tel">Телефон</label>
+            <input type="tel" class="form-control" id="order_tel" name="phone" placeholder="Телефон">
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="order_info">Параметры заказа</label>
+            <textarea class="form-control" id="order_info" name="window" rows="5" readonly="readonly"></textarea>
+        </div>
+        <br>
+        <span class="wnd_calc_captcha"><?php echo apply_filters('cptch_display', ''); ?></span>
+        <small class="help-block captcha" style="display:none">Неправильное значение<br></small>
+        <br>
+        <button type="submit" class="btn btn-primary" id="order_send" name="order_send" value="order_send">
+            Отправить
+        </button>
+    </form>
+</div>
+<!--</div>-->
